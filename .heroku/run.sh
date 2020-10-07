a16974e40ab5374074d384e8c88c3375b5f06cb4
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-10-07 23:27:37 +0530

    ...

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo apt-get install python3.8
-sudo apt-get install python3-pip
+apt-get install python3.8
+apt-get install python3-pip
+apt-get install pip3
 pip3 install tensorflow
-sudo apt-get install ffmpeg
-sudo apt-get install tesseract-ocr
+apt-get install ffmpeg
+apt-get install tesseract-ocr
 curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $BUILD_DIR/binaries/models/deepspeech-0.8.1-models.pbmm
 # curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $BUILD_DIR/binaries/models/deepspeech-0.8.1-models.scorer
