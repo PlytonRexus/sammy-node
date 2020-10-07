1da0d2d1c4fc79c3367758a13a5aca0bf2177567
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-10-08 00:06:39 +0530

    added 3 buildpacks

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-apt-get install python3.8
-apt-get install python3-pip
-apt-get install pip3
-pip3 install tensorflow
-apt-get install ffmpeg
-apt-get install tesseract-ocr
+# apt-get install python3.8
+# apt-get install python3-pip
+# apt-get install pip3
+# pip3 install tensorflow
+# apt-get install ffmpeg
+# apt-get install tesseract-ocr
 curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $BUILD_DIR/binaries/models/deepspeech-0.8.1-models.pbmm
 # curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $BUILD_DIR/binaries/models/deepspeech-0.8.1-models.scorer
