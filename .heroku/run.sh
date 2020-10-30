136ea805a722f7ec553925b21acb133a6bf382de
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-10-30 11:49:00 +0530

    ...

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -7,3 +7,4 @@
 # apt-get install tesseract-ocr
 # curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $BUILD_DIR/binaries/models/deepspeech-0.8.1-models.pbmm
 # curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $BUILD_DIR/binaries/models/deepspeech-0.8.1-models.scorer
+redis-server
