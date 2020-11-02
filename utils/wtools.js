63c63f0f6a3567ff20a1f2e1feca7c4d17d0894f
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-11-03 00:20:08 +0530

    http support added

--- utils/wtools.js
+++ utils/wtools.js
@@ -1,5 +1,5 @@
-const https = require ('https');
-const http = require ('http');
+let https = require ('https');
+let http = require ('http');
 const fs = require ('fs');
 const path = require ('path');
 
