5cfe409c62e9edbdbbd9648474c3e1558bf45f52
Mihir Jichkar <j.mihir19.mj@gmail.com>
2021-05-28 16:07:48 +0530

    feat:Modify start script to add environment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require ('http');
 const exp = require ('./exp');
 
 const server = http.createServer(exp);
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5001;
 
 server.listen(port, () => {
     console.log(`HTTP server active on port ${port}.`);
